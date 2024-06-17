d13728f392ba3d4c5f442c0d1d9ee804e2e9c173
Tomas <tomas.dambrauskass@gmail.com>
2024-06-17 18:38:26 +0300

    Test to build the images

--- backend/server.js
+++ backend/server.js
@@ -71,7 +71,7 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-  console.error("500 Server Error:", err.stack);
+  console.error("500 Server Error:", err.message);
   res.status(500).json({ error: "Server error" });
 });
 
