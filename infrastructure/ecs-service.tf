4aa4e057ee8cd750ef30de78e5ed57201ac43cae
Tomas <tomas.dambrauskass@gmail.com>
2024-07-15 18:26:20 +0300

    Testing the flow

--- infrastructure/ecs-service.tf
+++ infrastructure/ecs-service.tf
@@ -2,7 +2,7 @@ resource "aws_ecs_service" "example_service" {
   name            = "CA_Project_test_tf"
   cluster         = aws_ecs_cluster.ecs_cluster.arn
   task_definition = "${aws_ecs_task_definition.CA_Project.family}:${max(aws_ecs_task_definition.CA_Project.revision)}"
-  desired_count   = 0
+  desired_count   = 1
   launch_type     = "EC2"
 
   #   network_configuration {
